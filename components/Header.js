d8c0fc0c7ad43c8c905f17ef837be91999f4183d
Nico Garbaccio <garbaccio20@gmail.com>
2023-02-19 11:36:30 -0500

    Responsive search bar

--- components/Header.js
+++ components/Header.js
@@ -17,7 +17,7 @@ function Header() {
         </div>
         {/* Search */}
         <div className='flex items-center md:border-2 rounded-full py-2 md:shadow-sm'>
-            <input className='flex-grow pl-5 bg-transparent outline-none' type="text" placeholder="Start your search" />
+            <input className='flex-grow pl-5 bg-transparent outline-none text-sm text-gray-600 placeholder-gray-400' type="text" placeholder="Start your search" />
             <SearchIcon className='hidden md:inline-flex h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer md:mx-2' />
         </div>
         {/* Right */}
