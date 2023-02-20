12ecc234e066b13907910428f85db15208aee61c
Nico Garbaccio <garbaccio20@gmail.com>
2023-02-19 19:19:49 -0500

    Add number of guests and cancel search functions

--- components/Header.js
+++ components/Header.js
@@ -15,10 +15,15 @@ function Header() {
     const [searchInput, setSearchInput] = useState("");
     const [startDate, setStartDate] = useState(new Date())
     const [endDate, setEndDate] = useState(new Date())
+    const [noOfGuests, setNoOfGuests] = useState(1)
     
     const handleSelect = (ranges) => {
         setStartDate(ranges.selection.startDate)
         setEndDate(ranges.selection.endDate)
+    }
+
+    const resetInput = () => {
+        setSearchInput("")
     }
 
     const selectionRange = {
@@ -59,13 +64,29 @@ function Header() {
             </div>
         </div>
         {searchInput && (
-            <div>
+            <div className='flex flex-col col-span-3 mx-auto'>
                 <DateRangePicker
                     ranges={[selectionRange]}
                     minDate={new Date()} // So you can't search for dates in the past
                     rangeColors={["#FD5B61"]}
                     onChange={handleSelect}
                 />
+            <div className='flex items-center border-b mb-4'>
+                <h2 className='text-2xl flex-grow font-semibold'>Number of Guests</h2>
+                <UsersIcon className='h-5' />
+                <input 
+                    type="number" 
+                    value={noOfGuests}
+                    min={1}
+                    max={20}
+                    onChange={e => setNoOfGuests(e.target.value)}
+                    className='w-12 pl-2 text-lg outline-none text-red-400' 
+                />
+            </div>
+            <div className='flex'>
+                <button onClick={resetInput} className='flex-grow text-gray-500'>Cancel</button>
+                <button className='flex-grow text-red-400'>Search</button>
+            </div>
             </div>
         )}
     </header>
